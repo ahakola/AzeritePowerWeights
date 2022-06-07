56e13c77cb5e0c3702afcbb9e4dd4e5e4c7f9c53
Unknown <40722602+ahakola@users.noreply.github.com>
2022-06-07 16:53:42 +0300

    .toc bump

--- Core.lua
+++ Core.lua
@@ -3,7 +3,7 @@
 
 	Helps you pick the best Azerite powers on your gear for your class and spec.
 
-	(c) 2018 - 2020
+	(c) 2018 - 2022
 	Sanex @ EU-Arathor / ahak @ Curseforge
 ----------------------------------------------------------------------------]]--
 local ADDON_NAME, n = ...
